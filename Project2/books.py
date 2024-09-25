8ccfd2137d46e3e374b486cd95397ffcbd97dd50
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-09-25 15:52:26 +0100

    created some delete methods and update ethodss

--- Project2/books.py
+++ Project2/books.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from pygments.lexer import default
 
 app = FastAPI()
 
@@ -21,11 +22,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                'title': "A new book",
+                'author': "name of author",
+                'description': "A new description of the book",
+                'rating': 5
+            }
+        }
+    }
 
 
 BOOKS = [
@@ -42,6 +54,23 @@ BOOKS = [
 async def read_all_all_books():
     return BOOKS
 
+# path parameter
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+# query parameter
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            BOOKS.append(book)
+
+    return books_to_return
+
 # creates new book
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
@@ -49,6 +78,7 @@ async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
 
+# auto increment for book_id
 def find_book_id(book: Book):
     if len(BOOKS) > 0:
         book.id = BOOKS[-1].id + 1
@@ -57,3 +87,22 @@ def find_book_id(book: Book):
 
     return book
 
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    new_book = Book(**book.model_dump())
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id  == new_book.id:
+            BOOKS[i] = new_book
+
+@app.delete("/books/{book_id")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+"""
+Add a new field to Book and BookRequest called published_date: int (for example, published_date: int = 2012). So, this book as published on the year of 2012.
+Enhance each Book to now have a published_date
+Then create a new GET Request method to filter by published_date
+"""
