9c3bed29291563dc707f2746375ac918c6dcf2af
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-10-10 15:20:25 +0100

    changed to mysql

--- Project3/TodoApp/database.py
+++ Project3/TodoApp/database.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
+#sqlite - SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#postgresql - SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Mikasa02!@127.0.0.1:3306/TodoApplicationDatabase'
 
+
+#sqlite - engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# connect_args=({'check_same_thread': False}) only for sqlite
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
