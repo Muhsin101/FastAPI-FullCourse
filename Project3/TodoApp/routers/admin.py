9835e4848744ad4b09867f98ee5f10cbae2f4276
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-10-22 13:03:07 +0100

    fixed typo

--- Project3/TodoApp/routers/admin.py
+++ Project3/TodoApp/routers/admin.py
@@ -45,7 +45,7 @@ async def read_all(user: user_dependency, db: db_dependency):
     return db.query(Todos).all()
 
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delet_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=401, detail='Authentication failed')
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
