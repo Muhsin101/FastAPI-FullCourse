2673778866d43774c2b6944ad1418cc786083b25
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-10-24 15:20:54 +0100

    chnaged database connection

--- Project3/TodoApp/database.py
+++ Project3/TodoApp/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#sqlite - SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 #postgresql - SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Mikasa02!@127.0.0.1:3306/TodoApplicationDatabase'
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Mikasa02!@127.0.0.1:3306/TodoApplicationDatabase'
 
 
 #sqlite - engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
